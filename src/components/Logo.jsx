640ae16d1fc77ebeb1942af891576414a13592f1
Siddharth Rashiwala <srashiwala@yahoo.in>
2026-07-11 06:41:40 +0530

    refactor: remove unnecessary srcset attribute from logo image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png"
 
 function Logo({ width = "100px" }) {
   return <div style={{width}}>
-    <img src={logo} alt="logo" srcset="" />
+    <img src={logo} alt="logo" />
   </div>;
 }
 
